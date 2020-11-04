3357a94344b01bf1f0beff22ebf42d46dda14467
andrew <chibarandrey@gmail.com>
2020-11-04 12:48:45 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+    type Character {
+        id: ID!
+        name: String
+        status: String
+        gender: String
+        image: String
+    }
+    
+    type Query {
+        characters: [Character]
+        character(id: ID!): Character
+    }
+  `
+
+const data = require('./data')
+
+const resolvers = {
+  Query: {
+    characters: () => data,
+    character: (_, { id }) => {
+      return data.find( character => character.id == id )
+    }
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({ url }) => {
+  console.log(`Server is running at ${url}`)
+})
